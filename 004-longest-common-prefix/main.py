649c0cac8aae1d1892514c325112acb8c57c45f0
Mukhammadsaiid Norbaev mpro <norbaevmukhammadsaiid@gmail.com>
2026-02-23 18:34:36 +0000

    message from device 3

---
+++ 004-longest-common-prefix/main.py
@@ -0,0 +1,12 @@
+def longestCommonPrefix(strs) -> str:
+        prefix = strs[0]
+        print(prefix)
+        curr = ""
+        for i in range(len(strs)):
+            for j in range(i):
+                  curr
+                  pass
+            
+
+strs = ["bat","bag","bank","band"]
+longestCommonPrefix(strs)
