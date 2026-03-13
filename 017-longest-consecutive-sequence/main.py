b8dd85563ca2ddec659d1e7c730560b3d277c7fa
Mukhammadsaiid Norbaev <norbaevmukhammadsaiid@gmail.com>
2026-03-13 04:57:00 +0000

    updated, understood better the problem

--- 017-longest-consecutive-sequence/main.py
+++ 017-longest-consecutive-sequence/main.py
@@ -6,15 +6,26 @@ class Solution:
         if not nums:
             return 0
         nums.sort()
-        longest = 0
-        count = 1
+        longest, count = 0, 1
         for i in range(1, len(nums)):
-            if nums[i] - nums[i-1] == 1:
+            comparison = nums[i] - nums[i-1]
+            if comparison == 1:
                 count += 1
-            elif nums[i] - nums[i-1] == 0:
-                continue
-            elif nums[i] - nums[i-1] > 1:
+            elif comparison > 1:
                 longest = max(longest, count)
                 count = 1
-        
         return max(longest, count)
+    
+
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        numSet = set(nums)
+        longest = 0
+        count = 0
+        for n in nums:
+            if n-1 not in numSet:
+                count = 0
+                while n+count in numSet:
+                    count += 1
+                longest = max(longest, count)
+        return longest
