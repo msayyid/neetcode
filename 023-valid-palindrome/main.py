a0b70489a94bff1f85aa0e03ed85019835f5a337
msayyid <201980620+msayyid@users.noreply.github.com>
2026-03-22 00:13:43 +0000

    valid palindrome done, via different approaches

--- 023-valid-palindrome/main.py
+++ 023-valid-palindrome/main.py
@@ -5,15 +5,93 @@ class Solution:
         alph_nums = {
                 'a','b','c','d','e','f','g','h','i','j','k','l','m',
                 'n','o','p','q','r','s','t','u','v','w','x','y','z',
-                '0','1','2','3','4','5','6','7','8','9','0'}
+                '0','1','2','3','4','5','6','7','8','9'}
+        s_array = []
         s_only = ""
         for i in range(len(s)):
             if s[i].lower() in alph_nums:
-                s_only += s[i].lower()
-        s_back = ""
-        for i in range(len(s_only) - 1, -1, -1):
-            s_back += s_only[i]
+                s_array.append(s[i].lower())
+        # s_only = "".join(s_array)
+
+        s_b_array = []
+        # s_back = ""
+        for i in range(len(s_array) - 1, -1, -1):
+            s_b_array.append(s_array[i])
         
-        if s_back == s_only:
+        
+        if s_array == s_b_array:
             return True
         return False
+    
+## a little better
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        # numbers = "01234567890"
+        # alph_nums = "abcdefghijklmnopqrstuvwxyz01234567890"
+        alph_nums = {
+                'a','b','c','d','e','f','g','h','i','j','k','l','m',
+                'n','o','p','q','r','s','t','u','v','w','x','y','z',
+                '0','1','2','3','4','5','6','7','8','9'}
+        s_array = []
+        # s_only = ""
+        for i in range(len(s)):
+            if s[i].lower() in alph_nums:
+                s_array.append(s[i].lower())
+        # s_only = "".join(s_array)
+
+        # s_b_array = []
+        # # s_back = ""
+        # for i in range(len(s_array) - 1, -1, -1):
+        #     s_b_array.append(s_array[i])
+        return s_array == s_array[::-1]
+    
+## clean and beautiful
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s_array = []
+        for c in s:
+            if c.isalnum():
+                s_array.append(c.lower())
+        return s_array == s_array[::-1]
+    
+
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        l, r = 0, len(s) - 1
+        while l < r:
+            if s[l].isalnum() and s[r].isalnum():
+                if s[l].lower() != s[r].lower():
+                    return False
+                r -= 1
+                l += 1
+            elif s[l].isalnum() and not s[r].isalnum():
+                r -= 1
+            elif not s[l].isalnum() and s[r].isalnum():
+                l += 1
+            else:
+                r -= 1
+                l += 1
+        return True
+
+
+
+# better looking
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        l, r = 0, len(s) - 1
+        while l < r:
+            while l < r and not self.alpha_num(s[l]):
+                l += 1
+            while l < r and not self.alpha_num(s[r]):
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+            l, r = l + 1, r - 1
+        return True
+
+    def alpha_num(self, c):
+        return (ord("A") <= ord(c) <= ord("Z") or
+                ord("a") <= ord(c) <= ord("z") or
+                ord("0") <= ord(c) <= ord("9")
+                )
